9a007d91f306a3057e8ed45e82e660aeb2a87c57
Leandro Costa <lcostasantos1987@gmail.com>
2022-04-24 12:42:50 -0300

    Acumulando valores
    Acumulando valores

---
+++ LogicProg1/ex00015.js
@@ -0,0 +1,25 @@
+function pulaLinha() {
+    document.write("<br>");
+}
+
+function mostra(frase) {
+    document.write(frase);
+    pulaLinha();
+}
+
+var totalFamiliares = parseInt(prompt("Quantidade de Familieares?"));
+
+var numero = 1;
+
+var totalIdades = 0;
+
+while (numero  <= totalFamiliares) {
+    
+    var idade = parseInt(prompt("Informe idade do familiar"))
+    totalIdades = totalIdades + idade;
+    numero++;
+}
+
+var mediaDasIdades = totalIdades/totalFamiliares;
+mostra("A medida das idades dos Fami9liares é :" + mediaDasIdades);
+mostra("FIM")
