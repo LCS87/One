533bd572454ca37578cf4ebfd178957fee163581
Leandro Costa <lcostasantos1987@gmail.com>
2022-04-28 01:14:53 -0300

    Creeper
    creeper

---
+++ LogicProg2/Creeper.js
@@ -0,0 +1,24 @@
+        var tela = document.querySelector('canvas');
+        var pincel = tela.getContext('2d');
+
+        pincel.fillStyle = 'green';
+        pincel.fillRect( 0, 0, 350, 300 );
+        
+
+        pincel.fillStyle = 'black';
+        pincel.fillRect( 200, 50, 90, 90 );
+        
+
+        pincel.fillStyle = 'black';
+        pincel.fillRect( 50, 50, 90,90 );
+       
+
+        pincel.fillStyle = 'black';
+        pincel.fillRect( 135, 130, 70, 100 );
+      
+
+        pincel.fillStyle = 'black';
+        pincel.fillRect( 100, 180, 40, 110 );
+
+        pincel.fillStyle = 'black';
+        pincel.fillRect( 200, 180, 40,110 );
